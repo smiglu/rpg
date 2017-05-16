4991d6956e7d2ef86d2a775197604214bc25d67e
smiglu <jkb.smigielski@gmail.com>
2017-05-16 22:44:15 +0200

    poruszanie klawiatura i zablokowanie poruszania poza ekran

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,7 +1,13 @@
 'use strict';
-class Player extends PointObj{
+class Player extends PointObj {
     constructor(name, x, speedX, y, speedY) {
         super(name, x, speedX, y, speedY);
         this.hp = 20;
+        this.pressingDown = false;
+        this.pressingLeft = false;
+        this.pressingRight = false;
+        this.pressingUp = false;
+        this.width= 30;
+        this.height = 30;
     }
 }
